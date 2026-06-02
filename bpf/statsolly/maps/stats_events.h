e8a0c73c5648555c7cf93a95658943340bb40bf7
Giuseppe Ognibene <giuseppe.ognibene@coralogix.com>
2026-06-02 16:12:25 +0200

    Make stats ring buffer wakeup threshold configurable (#2198)

--- bpf/statsolly/maps/stats_events.h
+++ bpf/statsolly/maps/stats_events.h
@@ -14,7 +14,14 @@ struct {
     __uint(pinning, OBI_PIN_INTERNAL);
 } stats_events SEC(".maps");
 
+// To be injected from userspace during eBPF program load & initialization.
+// When 0, every submission wakes up userspace immediately.
+volatile const u32 stats_wakeup_data_bytes;
+
 static __always_inline long stats_events_flags() {
+    if (!stats_wakeup_data_bytes) {
+        return 0;
+    }
     const u64 avail_data = bpf_ringbuf_query(&stats_events, BPF_RB_AVAIL_DATA);
-    return avail_data >= 4096 ? BPF_RB_FORCE_WAKEUP : BPF_RB_NO_WAKEUP;
+    return avail_data >= stats_wakeup_data_bytes ? BPF_RB_FORCE_WAKEUP : BPF_RB_NO_WAKEUP;
 }
